07b21d622abae864f73216c01f1e246d40b0d0ca
Evgeny Bobkin <evgeny.bobkin@gmail.com>
2017-07-18 10:37:06 +0200

    initial setup

---
+++ script.js
@@ -0,0 +1,5 @@
+(function() {
+    const HOURHAND = document.querySelector("#hour");
+    const MINUTEHAND = document.querySelector("#minute");
+    const SECONDHAND = document.querySelector("#second");
+})();
