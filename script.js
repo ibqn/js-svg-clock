f75b03843cac339bbd38d23a21e63bbf65f3c7a7
Evgeny Bobkin <evgeny.bobkin@europace.de>
2022-12-25 16:29:47 +0100

    switch to sass

--- script.js
+++ script.js
@@ -1,46 +1,45 @@
-(function() {
-    const HOURHAND = document.querySelector("#hour");
-    const MINUTEHAND = document.querySelector("#minute");
-    const SECONDHAND = document.querySelector("#second");
+(function () {
+  const HOURHAND = document.querySelector("#hour");
+  const MINUTEHAND = document.querySelector("#minute");
+  const SECONDHAND = document.querySelector("#second");
 
-    let hourCor = 0;
-    let minuteCor = 0;
-    let secondCor = 0;
+  let hourCor = 0;
+  let minuteCor = 0;
+  let secondCor = 0;
 
+  const date = new Date();
+
+  let params = {
+    hour: date.getHours(),
+    minute: date.getMinutes(),
+    second: date.getSeconds(),
+  };
+
+  const rotFunc = (arg) => {
     const date = new Date();
 
-    let params = {
-        'hour': date.getHours(),
-        'minute': date.getMinutes(),
-        'second': date.getSeconds()
-    };
+    const hour = date.getHours();
+    const minute = date.getMinutes();
+    const second = date.getSeconds();
 
-    const rotFunc = (arg) => {
-        const date = new Date();
+    hourCor += arg.hour > hour ? 24 : 0;
+    minuteCor += arg.minute > minute ? 60 : 0;
+    secondCor += arg.second > second ? 60 : 0;
 
-        const hour = date.getHours();
-        const minute = date.getMinutes();
-        const second = date.getSeconds();
+    const hourPos = (hourCor + hour) * 30;
+    const minutePos = (minuteCor + minute) * 6;
+    const secondPos = (secondCor + second) * 6;
 
-        hourCor += (arg.hour > hour) ? 24 : 0;
-        minuteCor += (arg.minute > minute) ? 60 : 0;
-        secondCor += (arg.second > second) ? 60 : 0;
+    //console.log(`prev second ${arg.second} - new second ${second} - angle ${secondPos}`);
 
-        const hourPos = (hourCor + hour) * 30;
-        const minutePos = (minuteCor + minute) * 6;
-        const secondPos = (secondCor + second) * 6;
+    params.hour = hour;
+    params.minute = minute;
+    params.second = second;
 
-        //console.log(`prev second ${arg.second} - new second ${second} - angle ${secondPos}`);
+    HOURHAND.style.transform = `rotate(${hourPos}deg)`;
+    MINUTEHAND.style.transform = `rotate(${minutePos}deg)`;
+    SECONDHAND.style.transform = `rotate(${secondPos}deg)`;
+  };
 
-        params.hour = hour;
-        params.minute = minute;
-        params.second = second;
-
-        HOURHAND.style.transform = `rotate(${hourPos}deg)`;
-        MINUTEHAND.style.transform = `rotate(${minutePos}deg)`;
-        SECONDHAND.style.transform = `rotate(${secondPos}deg)`;
-    };
-
-    setInterval(rotFunc, 500, params);
-
+  setInterval(rotFunc, 500, params);
 })();
